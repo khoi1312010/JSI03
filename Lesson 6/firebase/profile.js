4655ccbba4a12d22c3929ab46dd7ba8edbbc17a9
khoi1312010 <th.khoi2010@gmail.com>
2023-09-17 18:03:53 +0700

    ok

--- Lesson 6/firebase/profile.js
+++ Lesson 6/firebase/profile.js
@@ -1,28 +1,8 @@
 import { auth } from './config.js';
-import { updateProfile, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { updateProfile } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 
 const name = document.getElementById('name');
 const photo = document.getElementById('photo');
-const submitBtn = document.getElementById('submit-btn');
+
 
 photo.src = photo.value;
-const handleUpdateProfile = () => {
-    updateProfile(auth.currentUser, {
-        displayName: name.value, photoURL: photo.value,
-      }).then(() => {
-        alert('Updated profile successfully')
-      }).catch((error) => {
-        alert(error.code)
-      });
-}
-
-const handleGetProfile = () => {
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-          console.log(user.displayName);
-        }
-      });
-    }
-
-submitBtn.addEventListener('click', handleUpdateProfile);
-handleGetProfile();
