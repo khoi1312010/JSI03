64411df4259a4a248222653095be2c307f169e7e
khoi1312010 <th.khoi2010@gmail.com>
2023-09-17 18:03:46 +0700

    ok

--- Lesson 6/firebase/login.js
+++ Lesson 6/firebase/login.js
@@ -1,59 +1,39 @@
+import { auth } from "./config.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { signInAnonymously } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 
-(function() {
-    console.log('Start file login with firebase');
-    var config = {
-        apiKey: "xxxxx",
-        authDomain: "xxxx.firebaseapp.com",
-        databaseURL: "xxxx.firebaseio.com",
-        projectId: "190786631722",
-        storageBucket: "xxxx.appspot.com",
-        messagingSenderId: "xxxxx"
-    };
-    firebase.initializeApp(config);
-    var database = firebase.database();
 
-    //Google singin provider
-    var ggProvider = new firebase.auth.GoogleAuthProvider();
-    //Facebook singin provider
-    var fbProvider = new firebase.auth.FacebookAuthProvider();
-    //Login in variables
-    const btnGoogle = document.getElementById('btnGoogle');
-    const btnFaceBook = document.getElementById('btnFacebook');
+const username = document.getElementById("username");
+const pass = document.getElementById("password");
+const loginBtn = document.getElementById("login-btn");
+const anonyBtn = document.getElementById("anonymous-btn")
+const handleLoginAnon = () => {
+  signInAnonymously(auth)
+  .then(() => {
+    window.location = "./index.html"
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+  });
+}
 
-        //Sign in with Google
-        btnGoogle.addEventListener('click', e => {
-            firebase.auth().signInWithPopup(ggProvider).then(function(result) {
-                var token = result.credential.accessToken;
-                var user = result.user;
-                console.log('User>>Google>>>>', user);
-                userId = user.uid;
+const HandleLogin = () => {
+  const email = username.value;
+  const password = pass.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+        alert(user.email)
+        window.location = './index.html'
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      alert(errorCode)
+    });
+};
 
-            }).catch(function(error) {
-                console.error('Error: handle error here>>>', error.code)
-            })
-        }, false)
-
-        //Sign in with Facebook
-        btnFaceBook.addEventListener('click', e => {
-            firebase.auth().signInWithPopup(fbProvider).then(function(result) {
-                // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-                var token = result.credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                console.log('User>>Facebook>', user);
-                // ...
-                userId = user.uid;
-            
-            }).catch(function(error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
-                console.error('Error: handle error here>Facebook>>', error.code)
-            });
-        }, false) 
-}())
+loginBtn.addEventListener("click", HandleLogin);
+anonyBtn.addEventListener("click", handleLoginAnon)
